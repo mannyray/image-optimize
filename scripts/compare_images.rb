995a54d2f5e703cc90badd07a86f367f05ebd688
Stan Z <nil>
2025-07-08 12:51:29 -0500

    fix typo

--- scripts/compare_images.rb
+++ scripts/compare_images.rb
@@ -323,7 +323,7 @@ html += <<~HTML
       const blob = new Blob([content], { type: 'text/plain' });
       const a = document.createElement('a');
       a.href = URL.createObjectURL(blob);
-      a.download = 'resuts.txt';
+      a.download = 'results.txt';
       a.click();
     }
 
